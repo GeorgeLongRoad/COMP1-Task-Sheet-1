385d5c8b2be9d513b1ea54f0860e1b72f94830ee
GeorgeLongRoad <31512@longroad.ac.uk>
2014-04-07 15:52:39 +0100

    Task 4 Completed
    formatted the recent scores table to make it look cleaner

--- Skeleton Program.py
+++ Skeleton Program.py
@@ -164,9 +164,11 @@ def DisplayRecentScores(RecentScores):
   print()
   print('Recent Scores: ')
   print()
-  for Count in range(1, NO_OF_RECENT_SCORES + 1):
-    print(RecentScores[Count].Name, 'got a score of', RecentScores[Count].Score)
+  print("{0:<10}{1:<10}".format("Name", "Score"))
   print()
+  for Count in range(1, NO_OF_RECENT_SCORES + 1):
+    print('{0:<10}{1:<10}'.format(RecentScores[Count].Name, RecentScores[Count].Score))
+  print() 
   print('Press the Enter key to return to the main menu')
   input()
   print()
